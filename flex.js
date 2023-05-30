1165eec2438db94c906f0fb5714060396d58f0da
James Unekwuojo <121576934+Jamesunekwuojo@users.noreply.github.com>
2023-05-30 22:53:11 +0100

    ICT assignment edit 1

---
+++ flex.js
@@ -0,0 +1,12 @@
+document.getElementById("line1").innerHTML = "Welome to our joint multi-innovational site"
+function myFunction() {
+    document.getElementById("line2").innerHTML="Our services of high standards"
+}
+//testing document.write
+document.write(5+6);
+document.write("<h3>Contact us @ our</h3>")
+document.write("<p>you can reach us on any of the media above</p>")
+//window.alert
+window.alert("Hellow world")
+//console.log
+console.log("hi");
